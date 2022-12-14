4ef5ba6051a29a22649ac832306e0dad9e4f5447
raj8679 <devraj8679@gmail.com>
2022-12-14 21:56:33 +0530

    added animation.jsx

---
+++ tasty-bite/src/Components/Animation.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import styles from "../Animation.module.css"
+
+const Animation = () => {
+  return (
+  <>
+    <div className={styles.anime_parent}>
+        <div className={styles.anime_div_1}></div>
+        <div className={styles.anime_div_2}></div>
+        <div className={styles.anime_div_3}></div>
+
+    </div>
+    </>
+  )
+}
+
+export default Animation;
